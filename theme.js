b37af556018260046dfc362899241e31bd7199cc
UserZYF <3466181911@qq.com>
2022-05-06 21:14:29 +0800

    增加roy的js脚本

--- theme.js
+++ theme.js
@@ -1,153 +1,211 @@
-window.自定义块标菜单 = {};
-
-const 注册自定义块标菜单 = function (块类型, 菜单文字,菜单图标, 回调函数) {
-  自定义块标菜单[块类型] ? null : (自定义块标菜单[块类型] = {});
-  自定义块标菜单[块类型][菜单文字]
-    ? null
-    : (自定义块标菜单[块类型][菜单文字] = {});
-  自定义块标菜单[块类型][菜单文字]["回调函数"] = 回调函数;
-  自定义块标菜单[块类型][菜单文字]["菜单文字"] = 菜单文字;
-  自定义块标菜单[块类型][菜单文字]["菜单图标"] = 菜单图标;
-
-};
-
-const 判定目标并添加菜单项目 = function (event) {
-  let 父元素 = event.target.parentElement;
-  if (父元素.getAttribute("draggable") == "true") {
-    扩展菜单(父元素);
-  } else if (父元素.parentElement.getAttribute("draggable") == "true") {
-    扩展菜单(父元素.parentElement);
+  /***js form Morgan***/
+  /****************************思源API操作**************************/ 
+  async function 设置思源块属性(内容块id, 属性对象) {
+    let url = '/api/attr/setBlockAttrs'
+    return 解析响应体(向思源请求数据(url, {
+        id: 内容块id,
+        attrs: 属性对象,
+    }))
   }
-};
-
-window.addEventListener("mousedown", 判定目标并添加菜单项目);
-
-const 扩展菜单 = function (父元素) {
-  if (自定义块标菜单[父元素.getAttribute("data-type")]) {
-    自定义块标菜单.当前块id = 父元素.getAttribute("data-node-id");
-    自定义块标菜单.当前块类型 = 父元素.getAttribute("data-type");
-    自定义块标菜单.全局菜单定时器 = setTimeout(注入列表菜单项目, 10);
+  async function 向思源请求数据(url, data) {
+    let resData = null
+    await fetch(url, {
+        body: JSON.stringify(data),
+        method: 'POST',
+        headers: {
+            Authorization: `Token ''`,
+        }
+    }).then(function (response) { resData = response.json() })
+    return resData
   }
-};
-
-const 注入列表菜单项目 = function () {
-  let 块标菜单 = document.getElementById("commonMenu");
-  let 最后项 = 块标菜单.querySelector(".b3-menu__item--readonly");
-  if (最后项) {
-    clearTimeout(自定义块标菜单.全局菜单定时器);
-
-    console.log(最后项);
-    console.log(自定义块标菜单);
-    for (let 菜单项目 in 自定义块标菜单[自定义块标菜单.当前块类型]) {
-      console.log(菜单项目);
-      菜单项目
-        ? 块标菜单.insertBefore(
-            生成列表菜单项目(
-              自定义块标菜单[自定义块标菜单.当前块类型][菜单项目]
-            ),
-            最后项
-          )
-        : null;
-    }
-  } else {
-    自定义块标菜单.全局菜单定时器 = setTimeout(注入列表菜单项目, 10);
+  async function 解析响应体(response) {
+    let r = await response
+    return r.code === 0 ? r.data : null
   }
-};
-
-const 生成列表菜单项目 = function (菜单项目) {
-  let button = document.createElement("button");
-  button.className = "b3-menu__item diy";
-  console.log(菜单项目);
-  button.onclick = () => 菜单项目.回调函数(自定义块标菜单.当前块id);
   
-  button.setAttribute("data-node-id", 自定义块标菜单.当前块id);
-  button.innerHTML = `<svg class="b3-menu__icon" style=""><use xlink:href="${菜单项目.菜单图标}"></use></svg><span class="b3-menu__label">${菜单项目.菜单文字}</span>`;
-  return button;
-};
+ 
+  /****UI****/
+  function ViewSelect(selectid,selecttype){
+  let button = document.createElement("button")
+  button.id="viewselect"
+  button.className="b3-menu__item"
+  button.innerHTML='<svg class="b3-menu__icon" style="null"><use xlink:href="#iconGlobalGraph"></use></svg><span class="b3-menu__label" style="">视图选择</span><svg class="b3-menu__icon b3-menu__icon--arrow" style="null"><use xlink:href="#iconRight"></use></svg></button>'
+  button.appendChild(SubMenu(selectid,selecttype))
+  return button
+}
 
-const 转换列表为导图 = function (块id) {
-  let blocks = document.querySelectorAll(
-    `.protyle-wysiwyg [data-node-id="${块id}"]`
-  );
-  if (blocks[0]) {
-    blocks.forEach((block) => block.setAttribute("custom-f", "dt"));
+function SubMenu(selectid,selecttype,className = 'b3-menu__submenu') {
+  let node = document.createElement('div');
+  node.className = className;
+  if(selecttype=="NodeList"){
+    node.appendChild(GraphView(selectid))
+    node.appendChild(TableView(selectid))
+	node.appendChild(kanbanView(selectid))
+    node.appendChild(DefaultView(selectid))
   }
-};
-const 转换列表为树状表格 = function (块id) {
-  let blocks = document.querySelectorAll(
-    `.protyle-wysiwyg [data-node-id="${块id}"]`
-  );
-  if (blocks[0]) {
-    blocks.forEach((block) => block.setAttribute("custom-f", "bg"));
+  if(selecttype=="NodeTable"){
+    node.appendChild(FixWidth(selectid))
+    node.appendChild(AutoWidth(selectid))
+	node.appendChild(Removeth(selectid))
+	node.appendChild(Defaultth(selectid))
   }
-};
-const 转换列表为默认视图 = function (块id) {
-  let blocks = document.querySelectorAll(
-    `.protyle-wysiwyg [data-node-id="${块id}"]`
-  );
-  if (blocks[0]) {
-    blocks.forEach((block) => block.setAttribute("custom-f", ""));
-  }
-};
-const 转换标题为子文档 = async function (块id) {
-  let 块数据 = null;
-  let data = {
-    stmt: `select * from blocks where id = '${块id}'`,
-  };
-  let resData = null;
-  await fetch("/api/query/sql", {
-    body: JSON.stringify(data),
-    method: "POST",
-    headers: {
-      Authorization: `Token `,
-    },
-  }).then(function (response) {
-    resData = response.json();
-  });
-  console.log(await resData)
-  块数据 =(await resData)["data"][0];
-  let data1 = {
-    pushMode: 0,
-    srcHeadingID: 块id,
-    targetNoteBook: 块数据.box,
-    targetPath: 块数据.path,
-  };
-  await fetch("/api/filetree/heading2Doc", {
-    body: JSON.stringify(data1),
-    method: "POST",
-    headers: {
-      Authorization: `Token `,
-    },
-  }).then()
-  let 块标菜单 = document.getElementById("commonMenu");
-  块标菜单.setAttribute("class","b3-menu fn__none")
-};
-const 运行代码块到文档 = function(块id){
-       let block = document.querySelector(
-        `.protyle-wysiwyg [data-node-id="${块id}"] div[contenteditable="true"]`
-      );
-      let oldscript =document.querySelector(`[data-src-id= "${块id}"]`)
-      oldscript?oldscript.remove():null
-      let script = document.createElement('script');
-      script.setAttribute('f', "dt");
-      script.setAttribute('data-src-id', 块id);
-      script.innerText=block.innerText
-      document.head.appendChild(script);
+return node;
 }
-const 应用代码块到文档 = function(块id){
-    let block = document.querySelector(
-     `.protyle-wysiwyg [data-node-id="${块id}"] div[contenteditable="true"]`
-   );
-   let oldstyle =document.querySelector(`[data-src-id= "${块id}"]`)
-   oldstyle?oldstyle.remove():null
-   let style = document.createElement('style');
-   style.setAttribute('data-src-id', 块id);
-   style.innerText=block.innerText
-   document.head.appendChild(style);
+
+function GraphView(selectid){
+  let button = document.createElement("button")
+  button.className="b3-menu__item"
+  button.setAttribute("data-node-id",selectid)
+  button.setAttribute("custom-attr-name","f")
+  button.setAttribute("custom-attr-value","dt")
+
+  button.innerHTML=`<svg class="b3-menu__icon" style=""><use xlink:href="#iconFiles"></use></svg><span class="b3-menu__label">转换为导图</span>`
+  button.onclick=ViewMonitor
+  return button
 }
-注册自定义块标菜单("NodeList", "显示为导图","#iconList", 转换列表为导图);
-注册自定义块标菜单("NodeList", "显示为树状表格","#iconList", 转换列表为树状表格);
-注册自定义块标菜单("NodeList", "显示为默认","#iconList", 转换列表为默认视图);
-注册自定义块标菜单("NodeHeading", "转换为子文档","#iconFile" ,转换标题为子文档);
-注册自定义块标菜单("NodeCodeBlock", "运行到当前窗口","#iconCode" ,运行代码块到文档);
-注册自定义块标菜单("NodeCodeBlock", "应用css到当前窗口","#iconCode" ,应用代码块到文档);
+function TableView(selectid){
+  let button = document.createElement("button")
+  button.className="b3-menu__item"
+  button.setAttribute("data-node-id",selectid)
+  button.setAttribute("custom-attr-name","f")
+  button.setAttribute("custom-attr-value","bg")
+
+  button.innerHTML=`<svg class="b3-menu__icon" style=""><use xlink:href="#iconTable"></use></svg><span class="b3-menu__label">转换为表格</span>`
+  button.onclick=ViewMonitor
+  return button
+}
+function kanbanView(selectid){
+  let button = document.createElement("button")
+  button.className="b3-menu__item"
+  button.setAttribute("data-node-id",selectid)
+  button.setAttribute("custom-attr-name","f")
+  button.setAttribute("custom-attr-value","kb")
+
+  button.innerHTML=`<svg class="b3-menu__icon" style=""><use xlink:href="#iconMenu"></use></svg><span class="b3-menu__label">转换为看板</span>`
+  button.onclick=ViewMonitor
+  return button
+}
+function DefaultView(selectid){
+  let button = document.createElement("button")
+  button.className="b3-menu__item"
+  button.onclick=ViewMonitor
+  button.setAttribute("data-node-id",selectid)
+  button.setAttribute("custom-attr-name","f")
+  button.setAttribute("custom-attr-value",'')
+
+  button.innerHTML=`<svg class="b3-menu__icon" style=""><use xlink:href="#iconList"></use></svg><span class="b3-menu__label">恢复为列表</span>`
+  return button
+}
+function FixWidth(selectid){
+  let button = document.createElement("button")
+  button.className="b3-menu__item"
+  button.onclick=ViewMonitor
+  button.setAttribute("data-node-id",selectid)
+  button.setAttribute("custom-attr-name","f")
+  button.setAttribute("custom-attr-value","")
+
+  button.innerHTML=`<svg class="b3-menu__icon" style=""><use xlink:href="#iconTable"></use></svg><span class="b3-menu__label">页面宽度</span>`
+  return button
+}
+function AutoWidth(selectid){
+  let button = document.createElement("button")
+  button.className="b3-menu__item"
+  button.setAttribute("data-node-id",selectid)
+  button.setAttribute("custom-attr-name","f")
+  button.setAttribute("custom-attr-value","auto")
+  button.innerHTML=`<svg class="b3-menu__icon" style=""><use xlink:href="#iconTable"></use></svg><span class="b3-menu__label">自动宽度</span>`
+  button.onclick=ViewMonitor
+  return button
+}
+function Removeth(selectid){
+  let button = document.createElement("button")
+  button.className="b3-menu__item"
+  button.onclick=ViewMonitor
+  button.setAttribute("data-node-id",selectid)
+  button.setAttribute("custom-attr-name","t")
+  button.setAttribute("custom-attr-value","biaotou")
+
+  button.innerHTML=`<svg class="b3-menu__icon" style=""><use xlink:href="#iconTable"></use></svg><span class="b3-menu__label">取消表头样式</span>`
+  return button
+}
+function Defaultth(selectid){
+  let button = document.createElement("button")
+  button.className="b3-menu__item"
+  button.setAttribute("data-node-id",selectid)
+  button.setAttribute("custom-attr-name","t")
+  button.setAttribute("custom-attr-value","")
+  button.innerHTML=`<svg class="b3-menu__icon" style=""><use xlink:href="#iconTable"></use></svg><span class="b3-menu__label">默认表头样式</span>`
+  button.onclick=ViewMonitor
+  return button
+}
+function MenuSeparator(className = 'b3-menu__separator') {
+  let node = document.createElement('button');
+  node.className = className;
+  return node;
+}
+
+/* 操作 */ 
+
+/**
+ * 获得所选择的块对应的块 ID
+ * @returns {string} 块 ID
+ * @returns {
+ *     id: string, // 块 ID
+ *     type: string, // 块类型
+ *     subtype: string, // 块子类型(若没有则为 null)
+ * }
+ * @returns {null} 没有找到块 ID */
+function getBlockSelected() {
+    let node_list = document.querySelectorAll('.protyle-wysiwyg--select');
+    if (node_list.length === 1 && node_list[0].dataset.nodeId != null) return {
+        id: node_list[0].dataset.nodeId,
+        type: node_list[0].dataset.type,
+        subtype: node_list[0].dataset.subtype,
+    };
+    return null;
+}
+
+function ClickMonitor () {
+  window.addEventListener('mouseup', MenuShow)
+}
+
+function MenuShow() {
+  setTimeout(() => {
+    let selectinfo = getBlockSelected()
+      if(selectinfo){
+      let selecttype = selectinfo.type
+      let selectid = selectinfo.id
+      if(selecttype=="NodeList"||selecttype=="NodeTable"){
+        setTimeout(()=>InsertMenuItem(selectid,selecttype), 0)
+      }
+    }
+  }, 0);
+}
+
+
+function InsertMenuItem(selectid,selecttype){
+  let commonMenu = document.getElementById("commonMenu")
+  let  readonly = commonMenu.querySelector(".b3-menu__item--readonly")
+  let  selectview = commonMenu.querySelector('[id="viewselect"]')
+  if(readonly){
+    if(!selectview){
+    commonMenu.insertBefore(ViewSelect(selectid,selecttype),readonly)
+    commonMenu.insertBefore(MenuSeparator(),readonly)
+    }
+  }
+}
+
+function ViewMonitor(event){
+  let id = event.currentTarget.getAttribute("data-node-id")
+  let attrName = 'custom-'+event.currentTarget.getAttribute("custom-attr-name")
+  let attrValue = event.currentTarget.getAttribute("custom-attr-value")
+  let blocks = document.querySelectorAll(`.protyle-wysiwyg [data-node-id="${id}"]`)
+  if(blocks){
+    blocks.forEach(block=>block.setAttribute(attrName,attrValue))
+  }
+  let attrs={}
+    attrs[attrName] =attrValue
+  设置思源块属性(id,attrs)
+}
+
+setTimeout(()=>ClickMonitor(),1000)
